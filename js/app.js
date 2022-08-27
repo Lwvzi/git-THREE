f14d62f61a47bca2ab061e44f7b49007dfe606bb
Lwvzi <lwazizozi@gmail.com>
2022-08-27 18:12:26 +0200

    created slider kohtml added bg-imgs, created img carousel + js funct & styled the imgs

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    //hero slider
+    $('#hero-slider').owlCarousel({
+        loop: true,
+        margin:0,
+        nav: true,
+        items:1,
+        smartSpeed: 1000,
+        navText: ['Prev', 'Next'],
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+});
