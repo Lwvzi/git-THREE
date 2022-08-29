d716e50e6fbd6fcddb25ba44ef8e19dec6dd9a2d
Lwvzi <lwazizozi@gmail.com>
2022-08-29 14:59:06 +0200

    created projects section with its slider, styled it on style sheet and made its function on app.js. Buttons not showing after 768 breakpoint

--- js/app.js
+++ js/app.js
@@ -6,18 +6,48 @@ $(document).ready(function(){
         margin:0,
         nav: true,
         items:1,
+        dots: false,
         smartSpeed: 1000,
         navText: ['Prev', 'Next'],
         responsive:{
             0:{
-                
+                nav: false,      //we are removing the btns for mobile view
             },
-            600:{
-                
-            },
-            1000:{
-                
+
+            768:{
+                nav: true,      //after 768 px(mobile), the btns return
             }
         }
-    })
+    });
+
+
+     //project slider
+     $('#project-slider').owlCarousel({
+        loop: true,
+        margin:0,
+        nav: true,
+        dots: false,
+        items: 2,
+        smartSpeed: 1000,
+        navText: ['Prev', 'Next'],
+        margin: 24,
+        responsive:{
+            0:{
+                items: 1,
+                margin: 0,
+            },
+
+            768:{
+                items: 2,
+                dots: true,
+            },
+
+            1140:{
+                center: true,
+                items: 2,
+                dots: true,
+            }
+        }
+    });
+
 });
